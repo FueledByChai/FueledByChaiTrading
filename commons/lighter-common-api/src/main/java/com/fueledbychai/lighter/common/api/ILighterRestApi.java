b1e10d652e8d7d3128aacc8d477dd1653f709172
FueledByChai <info@fueledbychai.com>
2026-02-15 22:41:27 -0800

    updated

--- commons/lighter-common-api/src/main/java/com/fueledbychai/lighter/common/api/ILighterRestApi.java
+++ commons/lighter-common-api/src/main/java/com/fueledbychai/lighter/common/api/ILighterRestApi.java
@@ -2,6 +2,8 @@ package com.fueledbychai.lighter.common.api;
 
 import com.fueledbychai.data.InstrumentDescriptor;
 import com.fueledbychai.data.InstrumentType;
+import com.fueledbychai.lighter.common.api.auth.LighterApiTokenResponse;
+import com.fueledbychai.lighter.common.api.auth.LighterCreateApiTokenRequest;
 
 public interface ILighterRestApi {
 
@@ -9,5 +11,9 @@ public interface ILighterRestApi {
 
     public InstrumentDescriptor getInstrumentDescriptor(String symbol);
 
-    
+    public LighterApiTokenResponse createApiToken(String authToken, LighterCreateApiTokenRequest request);
+
+    public LighterApiTokenResponse createApiToken(LighterCreateApiTokenRequest request);
+
+    public String getApiToken();
 }
